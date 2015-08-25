afedc686590da17d69cfddfab54c283f769db5ce
Abhinav Agarwalla <navabhi174@gmail.com>
2015-08-25 22:22:14 +0530

    Recipe Manager Complete

--- src/iikh/Database.java
+++ src/iikh/Database.java
@@ -4,71 +4,163 @@
  * and open the template in the editor.
  */
 package iikh;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
 /**
  *
- * @author user
+ * @author oop31
  */
+
 public class Database {
-    private static int rcount=0;
-    private static int mcount=0;
-    private static ArrayList<Recipe> r = new ArrayList<>();
-    private static Meal[] m;
+    String recipeName;
+    ArrayList<String> indegrients;
+    ArrayList<String> method;
     
-    public Database(){
+    private String parsetoString(String recipeName, ArrayList<String> indegrients, ArrayList<String> method){
+        String out = recipeName + ",,";
+        for(String x: indegrients)
+            out += (x + "::");
+        out += ",,";
+        for(String x: method)
+            out += (x + "::");
+        System.out.print( out );
+        return out;
     }
-
-    public static void editRecipe(int rid){
-        
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter 0 for editing name");
-        System.out.println("Enter 1 for editing ingredients");
-        System.out.println("Enter 2 for editing instructions");
-        int act = in.nextInt();
-        String redit=null;
-        if(act==0){
-            System.out.println("Enter new name");
-            String gr = in.nextLine();
-            redit = in.nextLine();
-            r.get(rid).setName(redit);
+    public void addRecipe(String recipeName, ArrayList<String> indegrients, ArrayList<String> method){
+        try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("recipeList.txt", true)))) {
+            out.println(parsetoString(recipeName, indegrients, method));  
+       //     System.out.print("ksadlvbw");
+        }catch (IOException e) {
+            e.printStackTrace();
+            //exception handling left as an exercise for the reader
         }
-        else if(act==1){
-            System.out.println("Enter the new ingredients");
-            String gr = in.nextLine();
-            redit = in.nextLine();
-            r.get(rid).setIngredients(redit);
-        }
-        else if(act==2){
-            System.out.println("Enter new instructions");
-            String gr = in.nextLine();
-            redit = in.nextLine();
-            r.get(rid).setInstruction(redit);
-        }
-        else{
-            System.out.println("Enter a valid input");
+     //   System.out.print("ksadlvbw");
+    }  
+    public ArrayList<String> repNames(){
+        BufferedReader br = null;
+        ArrayList<String> list = new ArrayList<String>();
+     //   System.out.println("here ");
+	try {
+            String line;
+            br = new BufferedReader(new FileReader("recipeList.txt"));
+          //  System.out.println("here ");
+            while ((line = br.readLine()) != null) {
+                String[] x = line.split(",,");
+                list.add(x[0]);
+            }
+	} catch (IOException e) {
+            e.printStackTrace();
+        }       
+      //  System.out.println("here ");
+        return list;       
+    }    
+    public void getRecipe(String recipeName){
+        BufferedReader br = null;
+        this.recipeName = null;
+        this.indegrients = null;
+        this.method = null;
+	try {
+            String line;
+            br = new BufferedReader(new FileReader("recipeList.txt"));
+            while ((line = br.readLine()) != null) {
+                String[] x = line.split(",,");
+                if(x[0].equals(recipeName)){
+                    this.recipeName = recipeName;
+                    this.indegrients = new ArrayList<String>(Arrays.asList(x[1].split("::")));
+                    this.method = new ArrayList<String>(Arrays.asList(x[2].split("::")));
+                }
+            }
+	} catch (IOException e) {
+            e.printStackTrace();
         }
     }
-    
-    public static void addRecipe(){
-        
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the name of Recipe:");
-        String rname = in.nextLine();
-        System.out.println("Enter the ingredients of Recipe:");
-        String ingr = in.nextLine();
-        System.out.println("Enter the instructions of Recipe:");
-        String inst = in.nextLine();
-
-        r.add(new Recipe(rname, ingr, inst));
-        rcount++;
+    public void editRecipe(String oldRecipeName, String newRecipeName, ArrayList<String> indegrients, ArrayList<String> method ){
+        try {
+            // Open the file that is the first
+            // command line parameter
+            FileInputStream fstream = new FileInputStream("recipeList.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            String strLine;
+            StringBuilder fileContent = new StringBuilder();
+            //Read File Line By Line
+            while ((strLine = br.readLine()) != null) {
+                // Print the content on the console
+              //  System.out.println(strLine);
+                String tokens[] = strLine.split(",,");
+                if (tokens.length > 0) {
+                    // Here tokens[0] will have value of ID
+                    if (tokens[0].equals(oldRecipeName)) {  
+                        System.out.print("here");
+                        String newLine = parsetoString(newRecipeName, indegrients, method);
+                        fileContent.append(newLine);
+                        fileContent.append("\n");
+                    } else {
+                        // update content as it is
+                        fileContent.append(strLine);
+                        fileContent.append("\n");
+                    }
+                }
+            }
+            // Now fileContent will have updated content , which you can override into file
+            FileWriter fstreamWrite = new FileWriter("recipeList.txt");
+            BufferedWriter out = new BufferedWriter(fstreamWrite);
+            out.write(fileContent.toString());
+            out.close();
+            //Close the input stream
+           // in.close();
+        } catch (Exception e) {//Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        }
     }
-    
-    public static void delRecipe(int rid){
-        r.remove(rid);
+    public void removeRecipe(String RecipeName ){
+        try {
+            // Open the file that is the first
+            // command line parameter
+            FileInputStream fstream = new FileInputStream("recipeList.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            String strLine;
+            StringBuilder fileContent = new StringBuilder();
+            //Read File Line By Line
+            while ((strLine = br.readLine()) != null) {
+                // Print the content on the console
+              //  System.out.println(strLine);
+                String tokens[] = strLine.split(",,");
+                if (tokens.length > 0) {
+                    // Here tokens[0] will have value of ID
+                    if (!tokens[0].equals(RecipeName)) {  
+                        // update content as it is
+                        fileContent.append(strLine);
+                        fileContent.append("\n");
+                    }
+                }
+            }
+            // Now fileContent will have updated content , which you can override into file
+            FileWriter fstreamWrite = new FileWriter("recipeList.txt");
+            BufferedWriter out = new BufferedWriter(fstreamWrite);
+            out.write(fileContent.toString());
+            out.close();
+            //Close the input stream
+           // in.close();
+        } catch (Exception e) {//Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        }
     }
-    public static void viewRecipe(int rid){
-        if(rid>r.size())System.out.println("RecipeID doesnt exist");
-        else System.out.println(r.get(rid).print());
+    public ArrayList<String> getIndegrients(){
+        return this.indegrients;
+    }
+    public ArrayList<String> getMethod(){
+        return this.method;
     }
 }
--- src/iikh/IIKH.java
+++ src/iikh/IIKH.java
@@ -15,54 +15,9 @@ public class IIKH {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Welcome to IIKH \n");
-        Scanner in = new Scanner(System.in);
         
-        while(true){
-            System.out.println("Enter 0 for adding recipe");
-            System.out.println("Enter 1 for editing recipe");
-            System.out.println("Enter 2 for viewing recipe");
-            System.out.println("Enter 3 for creating a plan");
-            System.out.println("Enter 4 for viewing a plan");
-            
-            Database recipeDB = new Database();
-            int action = in.nextInt();
-            System.out.println("You entered - " + action);
-            
-            switch(action){
-                case 0: {
-                    System.out.println("Adding Recipe");
-                    recipeDB.addRecipe();
-                    break;
-                }
-                
-                case 1: {
-                    System.out.println("Editing Recipe");
-                    
-                    System.out.println("Enter the Recipe id to be edited");
-                    int rid = in.nextInt();
-                    recipeDB.editRecipe(rid);
-                    break;
-                }
-                
-                case 2: {
-                    System.out.println("Viewing recipe");
-                    System.out.println("Enter the Recipe id to be viewed");
-                    int rid = in.nextInt();
-                    recipeDB.viewRecipe(rid);
-                    break;
-                }
-                    
-                case 3: System.out.println("Creating a plan");
-                        break;
-                
-                case 4: System.out.println("Viewing a plan");
-                        break;
-                
-                default: System.out.println("Select another option");
-            }
-        }
+        Greeter gt = new Greeter();
+        gt.setVisible(true);
     }
     
 }
